fecf846492731e2bb01f2a4ef5ec738784142c16
Francis Cirmi <cirmifrancis@gmail.com>
2024-03-23 18:48:20 -0300

    Primer commit

---
+++ clase1.py
@@ -0,0 +1,35 @@
+# Para correr el codigo: 'python3 script.py'
+
+# ======================================== CÓDIGO 1 ========================================
+
+# edad = 0
+
+# print('Python')
+# print(edad)
+# print(type(edad))
+
+# edad = int(input('Ingrese su edad: '))
+
+# #Casteo explicíto: le decimmos que tome a la variable 'edad' como un string
+# print('Tu edad es: ' + str(edad))
+# print('Tu edad es:', edad)
+
+# ======================================== CÓDIGO 2 ========================================
+
+# nombre = input('Ingrese su nombre: ')
+
+# print('Hola '+nombre)
+
+# ======================================== CÓDIGO 3 ========================================
+
+nota1 = 0
+nota2 = 0
+nota3 = 0
+promedio = 0
+
+nota1 = int(input('Ingrese la primera nota: '))
+nota2 = int(input('Ingrese la segunda nota: '))
+nota3 = int(input('Ingrese la tercera nota: '))
+promedio = (nota1+nota2+nota3) / 3
+
+print(promedio)
