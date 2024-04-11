58bd9603557e3e4b4f25a157ec86979042936c35
Francis Cirmi <cirmifrancis@gmail.com>
2024-04-11 11:32:37 -0300

    Codigo optimizado

--- game.py
+++ game.py
@@ -13,27 +13,15 @@ jugadaIA = random.choice(listaJugadas)
 print('La IA jugó: ' + jugadaIA)
 # print(type(jugadaIA))
     
-ganador = ''
+resultado = ''
 
-if jugadaUsuario=='Piedra' and jugadaIA=='Tijera':
-    ganador = 'Usuario'
-elif jugadaUsuario=='Piedra' and jugadaIA=='Papel':
-    ganador = 'IA'
-elif jugadaUsuario=='Piedra' and jugadaIA=='Piedra':
-    ganador = 'Nadie. Empate'
+if jugadaUsuario == jugadaIA:
+    resultado = 'Nadie. Empate'
+elif (jugadaUsuario=='Piedra' and jugadaIA=='Tijera') or \
+     (jugadaUsuario=='Papel' and jugadaIA=='Piedra') or \
+     (jugadaUsuario=='Tijera' and jugadaIA=='Papel'):
+    resultado = 'Usuario'
+else:
+    resultado = 'IA'
 
-if jugadaUsuario=='Papel' and jugadaIA=='Piedra':
-    ganador = 'Usuario'
-elif jugadaUsuario=='Papel' and jugadaIA=='Tijera':
-    ganador = 'IA'
-elif jugadaUsuario=='Papel' and jugadaIA=='Papel':
-    ganador = 'Nadie. Empate'
-
-if jugadaUsuario=='Tijera' and jugadaIA=='Papel':
-    ganador = 'Usuario'
-elif jugadaUsuario=='Tijera' and jugadaIA=='Piedra':
-    ganador = 'IA'
-elif jugadaUsuario=='Tijera' and jugadaIA=='Tijera':
-    ganador = 'Nadie. Empate'
-
-print('El ganador es... ' + ganador)
+print('El ganador es... ' + resultado)
