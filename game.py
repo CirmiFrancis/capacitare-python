5156b6f4305142b1609e7067d530ba78e388dc5f
Francis Cirmi <cirmifrancis@gmail.com>
2024-04-12 14:12:28 -0300

    Piedra - Papel - Tijera

--- game.py
+++ game.py
@@ -1,27 +1,56 @@
-# Piedra Papel Tijera
-
+# Piedra - Papel - Tijera
 import random
 
-print('¿Piedra, Papel o Tijera?')
-
-jugadaUsuario = input('Tu jugada es: ')
-# print(type(jugadaUsuario))
-
-listaJugadas = ['Piedra', 'Papel', 'Tijera']
-jugadaIA = random.choice(listaJugadas)
-
-print('La IA jugó: ' + jugadaIA)
-# print(type(jugadaIA))
-    
 resultado = ''
+victoriasUsuario = 0
+victoriasIA = 0
 
-if jugadaUsuario == jugadaIA:
-    resultado = 'Nadie. Empate'
-elif (jugadaUsuario=='Piedra' and jugadaIA=='Tijera') or \
-     (jugadaUsuario=='Papel' and jugadaIA=='Piedra') or \
-     (jugadaUsuario=='Tijera' and jugadaIA=='Papel'):
-    resultado = 'Usuario'
-else:
-    resultado = 'IA'
+while True:
+    print('===================================')
+    print('¿Piedra - Papel - Tijera? (Fin para salir)')
+    print('')
 
-print('El ganador es... ' + resultado)
+    bool = True
+    listaJugadas = ['Piedra', 'Papel', 'Tijera'] 
+
+    # Jugada Usuario
+    while True:
+        jugadaUsuario = input()
+        jugadaUsuario = jugadaUsuario.strip().capitalize()
+
+        if jugadaUsuario == 'Fin': 
+            bool = False
+            break
+        elif (jugadaUsuario not in listaJugadas):
+            print('Jugada no válida. Por favor, vuelve a escribir tu jugada.')
+            print('===================================')
+        else:
+            break
+
+    # Finalizar Ejecución
+    if not bool: 
+        print('===================================')
+        break
+
+    # Jugada IA
+    jugadaIA = random.choice(listaJugadas)
+
+    print('')
+    print('(Tú)', jugadaUsuario)
+    print('(IA)', jugadaIA)
+    print('')
+        
+    # Comparar Jugadas
+    if jugadaUsuario == jugadaIA:
+        resultado = 'Empate'
+    elif(jugadaUsuario == 'Piedra' and jugadaIA == 'Tijera') or \
+        (jugadaUsuario == 'Papel'  and jugadaIA == 'Piedra') or \
+        (jugadaUsuario == 'Tijera' and jugadaIA == 'Papel'):
+        resultado = 'Ganaste'
+        victoriasUsuario += 1
+    else:
+        resultado = 'Perdiste'
+        victoriasIA += 1
+
+    resultado = f'Resultado: {resultado} ({victoriasUsuario} - {victoriasIA})'
+    print(resultado)
