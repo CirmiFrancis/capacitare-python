8a223768f11f8acb19eb2cf714632e1cf5d9ed13
Francis Cirmi <cirmifrancis@gmail.com>
2024-06-12 01:40:22 -0300

    Codigo comentado. Trate de hacer una especia de prediccion pero no funciona bien. Hice un historial de jugadas.

--- juegos/prediccion.py
+++ juegos/prediccion.py
@@ -3,15 +3,18 @@
 
 # Colores
 red = "\033[91m"
+green = '\033[92m'
 yellow = "\033[93m"
 blue = "\033[94m"
 reset = "\033[0m"
 
 # Variables Globales
 jugadasPosibles = ['izquierda', 'derecha']
+historialJugadas = []
 contadorRondas = 1
 puntosUsuario = 0
 puntosIA = 0
+puntosMaximo = 10
 
 # Reglas
 def dar_reglas():
@@ -26,9 +29,14 @@ def jugada_usuario():
     jugadaUsuario = input(f'{blue}Tú: {reset}').strip().lower()
 
     while jugadaUsuario not in jugadasPosibles:
-        print(f'{yellow}Jugada inválida. Por favor, escribe nuevamente.{reset}\n')
-        jugadaUsuario = input(f'{blue}Tú: {reset}').strip().lower()
+        if jugadaUsuario == '/historial':
+            print(historialJugadas) # imprime todas las jugadas del usuario
+            jugadaUsuario = input(f'{blue}Tú: {reset}').strip().lower()
+        else:
+            print(f'{yellow}Jugada inválida. Por favor, escribe nuevamente.{reset}\n')
+            jugadaUsuario = input(f'{blue}Tú: {reset}').strip().lower()
 
+    historialJugadas.append(jugadaUsuario)
     return jugadaUsuario
 
 # Jugada IA
@@ -37,7 +45,31 @@ def jugada_ia():
 
     jugadaIA = jugadasPosibles[random.randrange(2)]
     print(f'{red}IA:{reset}', jugadaIA)
+
+    # if len(historialJugadas) < 4:
+    #     jugadaIA = jugadasPosibles[random.randrange(2)]
+    #     print(f'{red}IA:{reset}', jugadaIA)
+    # else:
+    #     jugadaIA = posibles_jugadas(historialJugadas)
+    #     if jugadaIA == None:
+    #         #print(f'{yellow}jugadaIA == None{reset}')
+    #         jugadaIA = jugadasPosibles[random.randrange(2)]
+    #     print(f'{red}IA:{reset}', jugadaIA)
     return jugadaIA
+
+# def posibles_jugadas(jugadas):
+#     if jugadas[-3:-1] == ['izquierda', 'izquierda']:
+#         #print(f'{yellow}primera jugada{reset}')
+#         return 'izquierda'
+#     if jugadas[-3:-1] == ['derecha', 'derecha']:
+#         #print(f'{yellow}segunda jugada{reset}')
+#         return 'derecha'
+#     if jugadas[-4:-1] == ['izquierda', 'derecha', 'izquierda']:
+#         #print(f'{yellow}tercera jugada{reset}')
+#         return 'derecha'
+#     if jugadas[-4:-1] == ['derecha', 'izquierda', 'derecha']:
+#         #print(f'{yellow}cuarta jugada{reset}')
+#         return 'izquierda'
 
 # Comparar Jugadas
 def comparar_jugadas(jugadaUsuario, jugadaIA):
@@ -62,9 +94,9 @@ def dar_resultado():
 # Programa Principal
 dar_reglas()
 
-while puntosUsuario < 10 and puntosIA < 10:
+while puntosUsuario < puntosMaximo and puntosIA < puntosMaximo:
     print('=============================================================')
-    print(f'{yellow}- RONDA {contadorRondas} -{reset}\n')
+    print(f'{yellow}- RONDA {contadorRondas} -{reset} (Escribe {green}/historial{reset} para ver todas las jugadas que hiciste hasta el momento.)\n')
     print(f'{blue}Tú ({puntosUsuario}){reset} - {red}IA ({puntosIA}){reset}\n')
     jugadaUsuario = jugada_usuario()
     jugadaIA = jugada_ia()
