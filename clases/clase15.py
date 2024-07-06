12eb37b89f36e78b54b3b1a4917c299028f995f4
Francis Cirmi <cirmifrancis@gmail.com>
2024-07-06 17:52:54 -0300

    Curso de Python terminado. Clase 14 modificada.

---
+++ clases/clase15.py
@@ -0,0 +1,4 @@
+# Clase 15 - Proyecto Integrador (E-Commerce), Parte 2
+
+# No fui, ya tenía hecho el proyecto.
+# Mencionaron 'Colorama' para agregar color en la consola.
---
+++ clases/clase16.py
@@ -0,0 +1,4 @@
+# Clase 16 - Proyecto Integrador (E-Commerce), Parte 3 + Presentaciones
+
+# Fui 2hs más tarde, ya tenía hecho el proyecto.
+# Pasamos 6 personas distintas para mostrar el proyecto terminado.
